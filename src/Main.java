3a6d30ec3ad29b42d7bb2e4868dd95324494eb13
bautifusti <bautijames@gmail.com>
2026-06-12 06:15:44 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -192,7 +192,6 @@ public class Main {
                 default: System.out.println("Opcion invalida.");
             }
         } while (op != 0);
-    }
 
     static void agregarPedido() {
         System.out.print("Codigo del producto: ");
